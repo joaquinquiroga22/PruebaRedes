2c9987e8e6bbbb67098f8efb33268d04046f9a7f
joaquinquiroga22 <joaquinquiroga22@gmail.com>
2022-03-21 14:37:26 -0300

    CambiosApi

--- src/Components/Facebook/Facebook.jsx
+++ src/Components/Facebook/Facebook.jsx
@@ -6,7 +6,7 @@ export default function Facebook() {
     const  [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch("https://graph.facebook.com/100049801391389?metadata=1&access_token=EAASZCmmPIGgABAOFIRgsaTIbak56Aue9wlcu1o0Y9D5fl9TZCEzlY9pRXNi4dxuEKQWBm5cut1jBjwW5dPK1UEWQ2sUvo59zRG00dS0aXquHhUS0spzmjzOY0doGPT0dmqjRvqf88JNKHoKDhkZAaePzM2LoZBbWPIWfTHYPjNmSyrannvsR0bKMhTIUuZCLz5aZBbsvZAgJI3jPuteqSaW")
+        fetch(`https://graph.facebook.com/search?q=auto&type=POST&access_token=EAASZCmmPIGgABAP7TgD8ZCWxHckuzujnzYZCfDltCBZCWVZBSjxtTfLX5XPlZBEHSCyBupAcPGxmQIodqpZAXWImAy1EZCPxNS4bD7N8mtmXTAKHZAmMAUteDRoL00oRJfau9HgF8M4sj53yj4IOOLqq9PPHoBZAx3znEpSGlL2BZAl73wzdNnlFXkZCyL6vShPQEiKgLiya7lIHu2uBA0GInvzA`)
          .then((res) => res.json())
          .then((data) =>{
              
